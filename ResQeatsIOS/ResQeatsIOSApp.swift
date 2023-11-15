f8c782da8e97c3fda0594fe7f569081667100224
raniahachem <rania.hachem@esprit.tn>
2023-11-15 16:05:40 +0100

    second commit

--- ResQeatsIOS/ResQeatsIOSApp.swift
+++ ResQeatsIOS/ResQeatsIOSApp.swift
@@ -13,7 +13,7 @@ struct ResQeatsIOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
